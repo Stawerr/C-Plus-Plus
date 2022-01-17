d2a97da42dc47787729e86937704c96976b9f13e
Stawerr <renato-silva_16@hotmail.com>
2022-01-17 23:17:24 +0000

    HorasEmSegundos

---
+++ Ler2HorasContarHorasTrabalho/Ler2HorasContarHorasTrabalho.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+int converterEntrada(int hora,int minutos,int segundos){
+    int soma = 0;
+    hora = 60 * 60 * hora;
+    minutos = 60 * minutos;
+    soma = hora + minutos + segundos;
+    return soma;
+}
+int intervalo(int entrada, int saida) {
+    int total=0;
+
+    total = saida - entrada;
+
+    return total;
+}
+int conversaoInversa(int intervaloTotal) {
+
+    return 0;
+}
+
+int main()
+{
+    setlocale(LC_ALL, ("portuguese"));
+
+    int horaE, minutoE, segundoE;
+    int horaS, minutoS, segundoS;
+
+    cout << "Introduza a hora de entrada HH MM SS";
+    cin >> horaE >> minutoE >> segundoE;
+
+    cout << "Introduza a hora de saida HH MM SS";
+    cin >> horaS >> minutoS >> segundoS;
+
+    int convertidoEntrada = converterEntrada(horaE, minutoE, segundoE);
+    int convertidosaida = converterEntrada(horaS, minutoS, segundoS);
+
+    int intervaloTotal = intervalo(convertidoEntrada, convertidosaida);
+
+    int intervaloFinal = conversaoInversa(intervaloTotal);
+
+}
+
